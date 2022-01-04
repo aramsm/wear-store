b5a6dd76ee5d054a85a38d5a5284b489a57ff4a0
Aram <aram.menocci@dito.com.br>
2022-01-04 19:23:44 -0300

    created new pkg

--- repository/repo.go
+++ repository/repo.go
@@ -1,4 +1,4 @@
-package server
+package repository
 
 import (
 	"encoding/json"
@@ -11,17 +11,20 @@ func ReadAndParseData() []Store {
 	bs, readErr := ioutil.ReadFile("acme-stores.json")
 
 	if readErr != nil {
-		fmt.Println("Error: ", readErr)
-		os.Exit(1)
+		logError(readErr)
 	}
 
 	var stores []Store
 	jsonErr := json.Unmarshal(bs, &stores)
 
 	if jsonErr != nil {
-		fmt.Println("Error: ", jsonErr)
-		os.Exit(1)
+		logError(jsonErr)
 	}
 
 	return stores
+}
+
+func logError(e error) {
+	fmt.Println("Error: ", e)
+	os.Exit(1)
 }
--- server/router.go
+++ server/router.go
@@ -1,11 +1,13 @@
 package server
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/aramsm/wear-store/controllers"
+	"github.com/gin-gonic/gin"
+)
 
-// Loads the routes configurations
 func ConfigRoutes(r *gin.Engine) *gin.Engine {
 	v1 := r.Group("api/v1")
-	stores := v1.Group("stores")
+	stores := v1.Group("stores", controllers.ShowStore)
 	addresses := v1.Group("addresses")
 	employees := v1.Group("employees")
 
